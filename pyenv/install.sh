14b40d7f0fe2c7114f27721a7a86f82d9b482ab8
Samuel Cristobal <el.tobal@gmail.com>
2023-06-10 16:36:53 +0200

    updates ubuntu

---
+++ pyenv/install.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+sudo apt update
+sudo apt install build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev curl \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # test whether the scripts shoudl run
-if [ "$(uname)" == Darwin ] || [[ -f /etc/debian_version ]]; then
+if [ "$(uname)" != Darwin ] && [[ ! -f /etc/debian_version ]]; then
     echo "Platform or OS not supported. Exiting"
     exit 1
 fi
@@ -24,7 +24,7 @@ if [ ! -f "$HOME/.antigen/antigen.zsh" ]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
         brew install antigen
     elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo apt-get update && sudo apt install -y zsh-antigen
+        curl -L git.io/antigen >antigen.zsh
     fi
 else
     echo "antigen is installed."
@@ -35,4 +35,4 @@ if [[ ! $($SHELL) == $(which zsh) ]]; then
     chsh -s "$(which zsh)"
 fi
 
-source "configure.sh"
+source "zsh/configure.sh"
