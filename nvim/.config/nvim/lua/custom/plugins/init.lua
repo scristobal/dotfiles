b3a1b8a8ba9f5884c4b83ae7a89698bfb963f983
Samuel Cristobal <el.tobal@gmail.com>
2024-01-28 22:20:15 +0100

    Removed submodule

---
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,20 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+-- relative line numbers
+vim.wo.number = true
+
+
+return {
+    -- NOTE: First, some plugins that don't require any configuration
+
+    -- transparent background
+    'tribela/vim-transparent',
+    -- 'xiyaowong/transparent.nvim',
+
+
+    -- Relative line numbers
+    'sitiom/nvim-numbertoggle',
+}
