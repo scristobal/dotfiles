536e558bb38d8b5d1bbc5e87c70c383d60c875ae
Samuel Cristobal <el.tobal@gmail.com>
2024-04-21 21:14:40 +0200

    Fix permissions

---
+++ helpers/install_alacritty_themes.sh
@@ -0,0 +1,3 @@
+# from https://github.com/alacritty/alacritty-theme/tree/master?tab=readme-ov-file#installation
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
