2b7a81eef629d30cee21d582b233ef2fdfacd704
Samuel Cristobal <el.tobal@gmail.com>
2024-04-22 20:00:30 +0200

    Moved mac stuff to its own folder

--- helpers/install_alacritty_themes.sh
+++
@@ -1,3 +0,0 @@
-# from https://github.com/alacritty/alacritty-theme/tree/master?tab=readme-ov-file#installation
-mkdir -p ~/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
