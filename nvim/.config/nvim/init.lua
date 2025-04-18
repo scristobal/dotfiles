38bf268d9da72729170f26680c86f37b7d36525d
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-04-18 19:24:47 +0200

    [nvim] neo-tree toggle keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,9 +23,3 @@ vim.schedule(function()
   require 'keymaps'
 end)
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-      vim.cmd("Neotree position=float")
-  end
-})
-
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '\\', ':Neotree toggle position=float<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
     popup_border_style = '',
