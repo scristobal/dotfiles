36e7eb2f5f0452b8fded7e78f2a638dd38afbd02
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-04-24 12:29:39 +0200

    fixup! [nvim] plugins sync

---
+++ nvim/.config/nvim/lua/plugins/actions.lua
@@ -0,0 +1,29 @@
+return {
+  'rachartier/tiny-code-action.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'folke/snacks.nvim',
+      opts = {
+        terminal = {},
+      },
+    },
+  },
+  event = 'LspAttach',
+  opts = {
+    backend = 'vim',
+    picker = 'snacks',
+  },
+  keys = {
+    {
+      '<leader>ca',
+      function()
+        require('tiny-code-action').code_action {}
+      end,
+      mode = { 'n', 'x' },
+      desc = 'Code action',
+      noremap = true,
+      silent = true,
+    },
+  },
+}
