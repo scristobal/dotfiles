69393e41eb863cc2635ba019db79a40774f9befd
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-05-02 07:29:27 +0200

    [nvim] outline.nvim

--- nvim/.config/nvim/lua/plugins/more.lua
+++ nvim/.config/nvim/lua/plugins/more.lua
@@ -28,7 +28,7 @@ return {
     opts = {},
   },
 
-  -- show code docs on a separate pannel
+  -- docs on a separate pannel
   -- {
   --   'amrbashir/nvim-docs-view',
   --   lazy = true,
@@ -38,6 +38,37 @@ return {
   --     width = 60,
   --   },
   -- },
+
+  -- code outline
+  {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = {
+      { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    },
+    opts = {
+      outline_window = {
+        position = "left",
+        width = 20,
+        relative_width = true,
+        auto_jump = true,
+      },
+      outline_items = {
+        show_symbol_details = false,
+      },
+      symbol_folding = {
+        autofold_depth = 2,
+        auto_unfold = {
+          hovered = true,
+          only = true,
+        },
+      },
+      preview_window = {
+        auto_preview = false,
+      },
+    }
+  },
 
   -- custom tabs
   -- {
