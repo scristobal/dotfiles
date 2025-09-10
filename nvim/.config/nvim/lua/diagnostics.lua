08b1dac4afbd581e8d5ae6d8b165c996b740cfec
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-09-10 07:07:30 +0200

    [nvim] subtle errors

--- nvim/.config/nvim/lua/diagnostics.lua
+++ nvim/.config/nvim/lua/diagnostics.lua
@@ -2,36 +2,26 @@ vim.diagnostic.config {
   severity_sort = true,
   float = { border = 'rounded', source = 'if_many' },
   underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      [vim.diagnostic.severity.WARN] = '󰀪 ',
-      [vim.diagnostic.severity.INFO] = '󰋽 ',
-      [vim.diagnostic.severity.HINT] = '󰌶 ',
-    },
-  },
-  virtual_text = {
-    source = 'if_many',
-    spacing = 2,
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
-  },
-}
-
-vim.diagnostic.config {
-  underline = true,
-  virtual_text = false,
+  -- signs = {
+  --   text = {
+  --     [vim.diagnostic.severity.ERROR] = '󰅚 ',
+  --     [vim.diagnostic.severity.WARN] = '󰀪 ',
+  --     [vim.diagnostic.severity.INFO] = '󰋽 ',
+  --     [vim.diagnostic.severity.HINT] = '󰌶 ',
+  --   },
+  -- },
+  virtual_text = true,
+  -- virtual_text = {
+  --   source = 'if_many',
+  --   spacing = 2,
+  -- },
   virtual_lines = false,
 }
 
 vim.keymap.set('n', '<leader>td', function()
   local virtual_lines = vim.diagnostic.config().virtual_lines
   vim.diagnostic.config { virtual_lines = not virtual_lines }
+
+  --   local virtual_text = vim.diagnostic.config().virtual_text
+  --   vim.diagnostic.config { virtual_text = not virtual_text }
 end, { desc = 'Toggle diagnostics' })
