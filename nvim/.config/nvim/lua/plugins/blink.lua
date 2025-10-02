33c130c7ceaafe65043e4c6fb847f78ee0763318
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-10-02 16:32:37 +0200

    [nvim] replace copilot-lsp with copilot.lua
    ...and getting rid of super broken and annoying NES thing

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -79,23 +79,23 @@ return {
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
-        ['<c-y>'] = {
-          function(cmp)
-            local nes = require 'copilot-lsp.nes'
-
-            if vim.b[vim.api.nvim_get_current_buf()].nes_state then
-              cmp.hide()
-              return (nes.apply_pending_nes() and nes.walk_cursor_end_edit())
-            end
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          'snippet_forward',
-          'fallback',
-        },
+        -- ['<c-y>'] = {
+        --   function(cmp)
+        --     local nes = require 'copilot-lsp.nes'
+        --
+        --     if vim.b[vim.api.nvim_get_current_buf()].nes_state then
+        --       cmp.hide()
+        --       return (nes.apply_pending_nes() and nes.walk_cursor_end_edit())
+        --     end
+        --     if cmp.snippet_active() then
+        --       return cmp.accept()
+        --     else
+        --       return cmp.select_and_accept()
+        --     end
+        --   end,
+        --   'snippet_forward',
+        --   'fallback',
+        -- },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
