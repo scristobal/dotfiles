2ae619d4df12e3e913686c5a156f73c1c8bb13bc
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-10-11 08:55:24 +0200

    [nvim] fix theme

--- nvim/.config/nvim/lua/themes/nordic.lua
+++ nvim/.config/nvim/lua/themes/nordic.lua
@@ -4,7 +4,11 @@ return {
   priority = 1000,
   config = function()
     require('nordic').load({
-      bright_border = true,
+       after_palette = function(palette)
+        local U = require("nordic.utils")
+        palette.bg_visual = U.blend(palette.orange.base, palette.bg, 0.15)
+    end,
+      bright_border = false,
     })
   end,
 }
