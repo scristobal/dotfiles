0d67d2e3ff753f994b36209da870d549cc3fc6c7
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-11-05 20:26:33 +0100

    Update minuet keybindings

--- nvim/.config/nvim/lua/plugins/minuet.lua
+++ nvim/.config/nvim/lua/plugins/minuet.lua
@@ -12,17 +12,13 @@ return {
           auto_trigger_ft = {},
           show_on_completion_menu = true,
           keymap = {
-            -- accept whole completion
-            accept = '<A-A>',
-            -- accept one line
-            accept_line = '<A-a>',
-            -- accept n lines (prompts for number)
-            -- e.g. "A-z 2 CR" will accept 2 lines
-            accept_n_lines = '<A-z>',
             -- Cycle to prev completion item, or manually invoke completion
             prev = '<A-[>',
             -- Cycle to next completion item, or manually invoke completion
             next = '<A-]>',
+            -- accept completion (whole)
+            accept = '<A-a>',
+            -- dismiss completion (all)
             dismiss = '<A-e>',
           },
         },
