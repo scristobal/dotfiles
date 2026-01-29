35a618e57ddee857cd1c105856cfbbb83a5bf399
Samuel Cristobal <samuel.cristobal@protonmail.com>
2026-01-29 07:36:27 +0100

    Transparent nvim bg

--- nvim/.config/nvim/lua/themes/vague.lua
+++ nvim/.config/nvim/lua/themes/vague.lua
@@ -1,8 +1,13 @@
 return {
   'vague2k/vague.nvim',
+  lazy = false,
+  priority = 1000,
   config = function()
-    require('vague').setup {
-      vim.cmd.colorscheme 'vague',
-    }
+    require('vague').setup({
+      transparent = true,
+      bold = false,
+      italic = false,
+    })
+    vim.cmd.colorscheme 'vague'
   end,
 }
