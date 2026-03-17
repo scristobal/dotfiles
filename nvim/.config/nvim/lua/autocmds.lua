d832045d8da44d7ea4f0fb946a7c666616f11f0a
Samuel Cristobal <samuel.cristobal@protonmail.com>
2026-03-17 07:44:07 +0100

    Simplify LSPs config

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,3 +1,70 @@
+-- LSP attach keymaps, highlights, and inlay hints
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('custom-lsp-attach', { clear = true }),
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    -- Attach navic for breadcrumbs in statusline
+    if client and client.server_capabilities.documentSymbolProvider then
+      require('nvim-navic').attach(client, event.buf)
+    end
+
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename', buffer = event.buf })
+    -- vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action', buffer = event.buf })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
+
+    -- this is shift-k by default
+    -- map('<leader>h', vim.lsp.buf.hover, 'Show [H]over info')
+
+    -- highlight references of the word under the cursor and clean up See `:help CursorHold`
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+      local highlight_augroup = vim.api.nvim_create_augroup('custom-lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('custom-lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'custom-lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
+    -- end of highlight references
+
+    -- toggle inlay hints if supported
+    local toggle_hints = function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+    end
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+      vim.keymap.set('n', '<leader>th', toggle_hints, { desc = 'Toggle inlay hints', buffer = event.buf })
+    end
+    -- end of toggle inlay hints
+
+    -- other useful keymaps
+    vim.keymap.set('n', '<C-space>', function()
+      vim.diagnostic.open_float()
+    end, { buffer = true, silent = true, desc = 'Show line diagnostics' })
+
+    vim.keymap.set('n', ']g', function()
+      vim.diagnostic.jump { count = 1, float = true }
+    end, { buffer = true, silent = true, desc = 'Jump to next diagnostic' })
+
+    vim.keymap.set('n', '[g', function()
+      vim.diagnostic.jump { count = -1, float = true }
+    end, { buffer = true, silent = true, desc = 'Jump to previous diagnostic' })
+  end,
+})
+
 -- notifications on LSP progress
 -- source `https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#-examples`
 
--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,117 +5,42 @@ return {
     dependencies = {
       -- grabs json schemas from SchemaStore
       { 'b0o/schemastore.nvim' },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('custom-lsp-attach', { clear = true }),
-        callback = function(event)
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-          -- Attach navic for breadcrumbs in statusline
-          if client and client.server_capabilities.documentSymbolProvider then
-            require('nvim-navic').attach(client, event.buf)
-          end
-
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename', buffer = event.buf })
-          -- vim.keymap.set({ 'n', 'x' }, '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action', buffer = event.buf })
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
-
-          -- this is shift-k by default
-          -- map('<leader>h', vim.lsp.buf.hover, 'Show [H]over info')
-
-          -- highlight references of the word under the cursor and clean up See `:help CursorHold`
-          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('custom-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('custom-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'custom-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-          -- end of highlight references
-
-          -- toggle inlay hints if supported
-          local toggle_hints = function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-          end
-          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            vim.keymap.set('n', '<leader>th', toggle_hints, { desc = 'Toggle inlay hints', buffer = event.buf })
-          end
-          -- end of toggle inlay hints
-
-          -- other useful keymaps
-          vim.keymap.set('n', '<C-space>', function()
-            vim.diagnostic.open_float()
-          end, { buffer = true, silent = true, desc = 'Show line diagnostics' })
-
-          vim.keymap.set('n', ']g', function()
-            vim.diagnostic.jump { count = 1, float = true }
-          end, { buffer = true, silent = true, desc = 'Jump to next diagnostic' })
-
-          vim.keymap.set('n', '[g', function()
-            vim.diagnostic.jump { count = -1, float = true }
-          end, { buffer = true, silent = true, desc = 'Jump to previous diagnostic' })
-        end,
-      })
-
-      -- this could be moved to /lsp folder, worth it?
-      local servers = {
-        cspell_ls = {},
-        clangd = {},
-        fortls = {},
-        rust_analyzer = {
-          settings = {
-            ['rust-analyzer'] = {
-              cargo = {
-                loadOutDirsFromCheck = true,
-                runBuildScripts = true,
-                allFeatures = true,
-              },
-              -- check = {
-              --   command = 'clippy',
-              --   extraArgs = {
-              --     '--no-deps',
-              --     '--all-features',
-              --     '--',
-              --     '-Dclippy::correctness',
-              --     '-Dclippy::complexity',
-              --     '-Wclippy::perf',
-              --     '-Wclippy::pedantic',
-              --   },
-              -- },
-              procMacro = {
-                enable = true,
+      {
+        'mason-org/mason-lspconfig.nvim',
+        opts = {
+          ensure_installed = {},
+          automatic_enable = true,
+        },
+        dependencies = {
+          {
+            'mason-org/mason.nvim',
+            opts = {
+              ui = {
+                border = 'rounded',
               },
             },
           },
         },
+      },
+    },
+    config = function()
+      local servers = {
+        -- cspell_ls = {},
+        -- codebook = {},
+        rust_analyzer = {},
         gleam = {
           -- uncomment to use debug LSP
           -- cmd = { '/home/samu/repos/forks/gleam/target/debug/gleam', 'lsp' },
           -- cmd = { '/Users/samu/repos/forks/gleam/target/debug/gleam', 'lsp' },
         },
-        ts_ls = {
-          -- root_dir = require('lspconfig').util.root_pattern { 'tsconfig.json', 'jsconfig.json', 'package.json' },
-          -- eoot_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json' },
-          -- workspace_required = true,
-          -- single_file_support = false,
-          -- settings = {},
-        },
+        tsgo = {},
+        -- ts_ls = {
+        --   root_dir = require('lspconfig').util.root_pattern { 'tsconfig.json', 'jsconfig.json', 'package.json' },
+        --   eoot_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json' },
+        --   workspace_required = true,
+        --   single_file_support = false,
+        --   settings = {},
+        -- },
         -- denols = {
         --   root_dir = require('lspconfig').util.root_pattern { 'deno.json', 'deno.jsonc' },
         --   root_markers = { 'deno.json', 'deno.jsonc' },
@@ -129,9 +54,12 @@ return {
         kotlin_lsp = {},
         jsonls = { settings = { json = { schemas = require('schemastore').json.schemas(), validate = { enable = true } } } },
         taplo = {},
-        -- pest_ls = {},
-        -- ruff_lsp = {},
+        clangd = {},
+        -- fortls = {},
         -- pyright = {},
+        -- ty = {},
+        -- ruff_lsp = {},
+        -- pest_ls = {},
         -- gopls = {},
         -- zls = {},
       }
